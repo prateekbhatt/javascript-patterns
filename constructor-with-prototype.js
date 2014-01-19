21edd9e65d8a325934fd6163adcab5ccdb870a40
Prateek Bhatt <prattbhatt@gmail.com>
2014-01-19 15:58:46 +0530

    constructor pattern  using the prototype object

---
+++ constructor-with-prototype.js
@@ -0,0 +1,18 @@
+// A car 'class'
+function Car (model) {
+    this.model = model;
+    this.color = 'silver';
+    this.year = 2012;
+}
+
+// add a getInfo method to the prototype method
+Car.prototype.getInfo = function () {
+    return this.model + ' ' + this.year;
+}
+
+// Instantiate an object using the Car constructor defined above
+var myCar = new Car('tesla');
+myCar.year = 2014;
+console.log(myCar.getInfo());
+
+// A single instance of getInfo is shared betweem all of the Car objects
